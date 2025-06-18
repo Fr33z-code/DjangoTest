30239f4ecbd0b78339dfdf9ba6022ed354b41e2a
perevozshikov <d0llar0v@yandex.ru>
2025-06-18 15:19:38 +0300

    /

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -3,7 +3,6 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from app1 import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', views.signup_view, name='signup'),
@@ -11,5 +10,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('catalog/', views.catalog, name='catalog'),
     path('profile/', views.profile_view, name='profile'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', views.catalog, name='catalog')
 ]
